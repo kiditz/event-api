0992751fbe5c7f4bc87f9e0408f26ba10013fd42
kiditz <kiditzbastara@gmail.com>
2020-07-11 23:24:18 +0700

    add data field

--- translate/error.go
+++ translate/error.go
@@ -5,5 +5,6 @@ type ErrorModel struct {
 	Status     string      `json:"status"`
 	StatusCode int         `json:"status_code"`
 	Errors     interface{} `json:"errors,omitempty"`
+	Data       interface{} `json:"data,omitempty"`
 	Message    string      `json:"message,omitempty"`
 }
