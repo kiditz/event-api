73ee3e2f65cc9706568643b973c48d9e24965f35
kiditz <kiditzbastara@gmail.com>
2020-07-16 01:14:23 +0700

    Add company location

--- entity/company.go
+++ entity/company.go
@@ -2,10 +2,11 @@ package entity
 
 // Company is the employer company for create and sharing job
 type Company struct {
-	ID          uint   `json:"id" gorm:"primary_key"`
-	Name        string `json:"name" validate:"required"`
-	Description string `json:"description" validate:"min=30"`
-	Location    string `json:"location" validate:"required"`
-	Industry    string `json:"industry" validate:"required"`
+	ID          uint     `json:"id" gorm:"primary_key"`
+	Name        string   `json:"name" validate:"required"`
+	Description string   `json:"description" validate:"min=30"`
+	Location    Location `json:"location" validate:"required"`
+	LocationID  uint     `json:"location_id,omitempty"`
+	Industry    string   `json:"industry" validate:"required"`
 	Model
 }
