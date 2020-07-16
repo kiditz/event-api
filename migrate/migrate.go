3e938acd7f0d8a4bf820d9b131f348f3dc0c773f
kiditz <kiditzbastara@gmail.com>
2020-07-16 13:31:30 +0700

    Add location to migrate

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -11,7 +11,9 @@ func main() {
 	db.DB.AutoMigrate(
 		&e.Job{},
 		&e.Company{},
+		&e.Location{},
 	)
 	db.DB.Model(&e.Job{}).AddForeignKey("company_id", "companies(id)", "CASCADE", "CASCADE")
+	db.DB.Model(&e.Company{}).AddForeignKey("location_id", "locations(id)", "CASCADE", "CASCADE")
 	db.DB.Model(&e.Job{}).AddUniqueIndex("idx_company_id_title", "company_id", "title", "status")
 }
