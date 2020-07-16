462e2731cc668e27bbd1734e8ee8e3207674a5ed
kiditz <kiditzbastara@gmail.com>
2020-07-16 13:30:42 +0700

    Handle company location load by city and country

--- entity/company.go
+++ entity/company.go
@@ -5,7 +5,7 @@ type Company struct {
 	ID          uint     `json:"id" gorm:"primary_key"`
 	Name        string   `json:"name" validate:"required"`
 	Description string   `json:"description" validate:"min=30"`
-	Location    Location `json:"location" validate:"required"`
+	Location    Location `json:"location,omitempty" validate:"required"`
 	LocationID  uint     `json:"location_id,omitempty"`
 	Industry    string   `json:"industry" validate:"required"`
 	Model
