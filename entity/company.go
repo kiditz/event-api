fd597fe4f1bb46642283ca682563313128d30405
kiditz <kiditzbastara@gmail.com>
2020-07-16 18:11:09 +0700

    Add field website and image url

--- entity/company.go
+++ entity/company.go
@@ -4,8 +4,11 @@ package entity
 type Company struct {
 	ID          uint     `json:"id" gorm:"primary_key"`
 	Name        string   `json:"name" validate:"required"`
-	Description string   `json:"description" validate:"min=30"`
-	Location    Location `json:"location,omitempty" validate:"required"`
+	Website     string   `json:"website,omitempty"`
+	ImageURL    string   `json:"image_url,omitempty"`
+	Description string   `gorm:"type:varchar(300);not null" json:"description" validate:"required,min=30,max=300"`
+	Overview    string   `json:"overview"`
+	Location    Location `json:"location,omitempty"`
 	LocationID  uint     `json:"location_id,omitempty"`
 	Industry    string   `json:"industry" validate:"required"`
 	Model
