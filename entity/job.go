6f00f6c0e0da359330acb3fee60e4d44b229d022
kiditz <kiditzbastara@gmail.com>
2020-07-16 18:11:36 +0700

    Make description not null

--- entity/job.go
+++ entity/job.go
@@ -6,7 +6,7 @@ import "time"
 type Job struct {
 	ID          uint      `gorm:"primary_key" json:"id"`
 	Title       string    `sql:"index" gorm:"type:varchar(100);index:title_idx;" json:"title" validate:"required"`
-	Description string    `gorm:"required" json:"description" validate:"required,min=30"`
+	Description string    `gorm:"not null" json:"description" validate:"required,min=30"`
 	TalentNum   int       `gorm:"not null" json:"talent_num" validate:"required,gte=1"`
 	Status      string    `sql:"index" gorm:"type:varchar(10);" json:"status,omitempty"`
 	StartDate   time.Time `gorm:"not null" json:"start_date" validate:"required"`
