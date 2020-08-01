cb093cd1b5615d86fd45bc6a865fe4060c99226a
kiditz <kiditzbastara@gmail.com>
2020-08-01 17:04:21 +0700

    Handle module login

--- controller/user.go
+++ controller/user.go
@@ -1,12 +1,15 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	e "github.com/kiditz/spgku-api/entity"
 	r "github.com/kiditz/spgku-api/repository"
 	t "github.com/kiditz/spgku-api/translate"
-	"github.com/kiditz/spgku-api/utils"
+	u "github.com/kiditz/spgku-api/utils"
 	"github.com/labstack/echo/v4"
 	"github.com/lib/pq"
 )
@@ -22,7 +25,12 @@ func AddUser(c echo.Context) error {
 	if hasErr != nil {
 		return t.Errors(c, http.StatusBadRequest, hasErr)
 	}
-	user.CreatedBy = utils.GetUsername(c)
+	user.CreatedBy = u.GetUsername(c)
+	pwd, err := u.HashAndSalt([]byte(user.Password))
+	if err != nil {
+		return t.Errors(c, http.StatusBadRequest, err.Error())
+	}
+	user.Password = pwd
 	err = r.AddUser(user)
 	if err != nil {
 		if err, ok := err.(*pq.Error); ok {
@@ -32,4 +40,29 @@ func AddUser(c echo.Context) error {
 		return t.Errors(c, http.StatusBadRequest, err.Error())
 	}
 	return t.Success(c, user)
+}
+
+//SignIn used to login
+func SignIn(c echo.Context) error {
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+	user, err := r.FindUserByEmail(email)
+	if err != nil {
+		return t.Errors(c, http.StatusBadRequest, err.Error())
+	}
+	fmt.Printf("Name : %v", user.Name)
+	if u.ComparePasswords(user.Password, []byte(password)) {
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = user.Name
+		claims["email"] = user.Email
+		claims["type"] = user.Type
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+		result, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return err
+		}
+		return t.Success(c, map[string]string{"token": result})
+	}
+	return t.Errors(c, http.StatusUnauthorized, t.TranslateString(c, "user_not_found"))
 }
