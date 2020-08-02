651e8119100493bf8f154af0e5dae779e90b9dbf
kiditz <kiditzbastara@gmail.com>
2020-08-02 15:58:21 +0700

    Rename digital staff controller to staff and add field followers rate

--- entity/digital_staf.go
+++ entity/digital_staf.go
@@ -2,9 +2,10 @@ package entity
 
 //DigitalStaff is category for digital staffing
 type DigitalStaff struct {
-	ID       uint   `gorm:"primary_key" json:"id"`
-	Title    string `sql:"index" gorm:"type:varchar(255);index:title_idx;" json:"title" validate:"required"`
-	Subtitle string `gorm:"type:varchar(100);not null" json:"subtitle" validate:"required"`
-	Image    string `gorm:"not null" json:"image" validate:"required"`
+	ID        uint   `gorm:"primary_key" json:"id"`
+	Title     string `sql:"index" gorm:"type:varchar(255);" json:"title" validate:"required"`
+	Subtitle  string `gorm:"type:varchar(100);not null" json:"subtitle" validate:"required"`
+	Image     string `gorm:"not null" json:"image" validate:"required"`
+	Followers string `gorm:"type:varchar(100);" json:"followers"`
 	Model
 }
