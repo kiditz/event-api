c7e4e59ef464576b4831a363f9884b068d58e832
kiditz <kiditzbastara@gmail.com>
2020-08-02 15:49:49 +0700

    Add image for digital staf and event staf

--- entity/digital_staf.go
+++ entity/digital_staf.go
@@ -3,7 +3,8 @@ package entity
 //DigitalStaff is category for digital staffing
 type DigitalStaff struct {
 	ID       uint   `gorm:"primary_key" json:"id"`
-	Title    string `sql:"index" gorm:"type:varchar(100);index:title_idx;" json:"title" validate:"required"`
-	Subtitle string `gorm:"not null" json:"subtitle" validate:"required"`
+	Title    string `sql:"index" gorm:"type:varchar(255);index:title_idx;" json:"title" validate:"required"`
+	Subtitle string `gorm:"type:varchar(100);not null" json:"subtitle" validate:"required"`
+	Image    string `gorm:"not null" json:"image" validate:"required"`
 	Model
 }
