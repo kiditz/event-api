9bdfc76d1f24cf938bba941b74dd5172a99cf78c
kiditz <kiditzbastara@gmail.com>
2020-08-07 12:53:05 +0700

    Handle upload files

--- controller/document.go
+++ controller/document.go
@@ -1,12 +1,19 @@
 package controller
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"time"
 
+	"cloud.google.com/go/storage"
 	e "github.com/kiditz/spgku-api/entity"
 	r "github.com/kiditz/spgku-api/repository"
 	t "github.com/kiditz/spgku-api/translate"
+	"github.com/kiditz/spgku-api/utils"
 	"github.com/labstack/echo/v4"
 )
 
@@ -34,19 +41,47 @@ func AddDocument(c echo.Context) error {
 		return t.Errors(c, http.StatusInternalServerError, err.Error())
 	}
 	defer src.Close()
+	bucket := os.Getenv("BUCKET_NAME")
+	object := utils.GetEmail(c) + "/" + file.Filename
+	message := uploadFile(bucket, object, src)
 
-	dst, err := os.Create(file.Filename)
-	if err != nil {
-		return err
-	}
-	defer dst.Close()
 	document := &e.Document{
 		FileName: file.Filename,
 		Size:     file.Size,
+		URL:      "storage.googleapis.com/" + bucket + "/" + object,
+		Message:  message.Error(),
 	}
 	err = r.AddDocument(document)
 	if err == nil {
 		return t.Success(c, document)
 	}
 	return t.Errors(c, http.StatusInternalServerError, map[string]string{"error": http.StatusText(http.StatusInternalServerError)})
+}
+func uploadFile(bucket string, object string, src multipart.File) error {
+	// bucket := "bucket-name"
+	// object := "object-name"
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %v", err)
+	}
+	defer client.Close()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := client.Bucket(bucket).Object(object).NewWriter(ctx)
+	if _, err = io.Copy(wc, src); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	acl := client.Bucket(bucket).Object(object).ACL()
+	if err := acl.Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return err
+	}
+	return fmt.Errorf("Blob %s uploaded", object)
 }
