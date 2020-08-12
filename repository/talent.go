85ba8b5728b1f9ef4543e164dc770fe5ed0f4e21
kiditz <kiditzbastara@gmail.com>
2020-08-12 12:49:15 +0700

    Handle filtered talent

--- repository/talent.go
+++ repository/talent.go
@@ -27,12 +27,12 @@ func FindTalentByID(talentID int) (e.Talent, error) {
 
 // FilteredTalent used to filter talent query
 type FilteredTalent struct {
-	CategoryID    int64  `query:"category_id"`
-	ExpertiseName string `query:"expertise_name"`
-	Limit         int64  `query:"limit"`
-	Offset        int64  `query:"offset"`
-	Q             string `query:"q"`
-	SubCategoryID int64  `query:"sub_category_id"`
+	CategoryID    int64  `query:"category_id" json:"category_id"`
+	ExpertiseName string `query:"expertise_name" json:"expertise_name"`
+	Limit         int64  `query:"limit" json:"limit"`
+	Offset        int64  `query:"offset" json:"offset"`
+	Q             string `query:"q" json:"q"`
+	SubCategoryID int64  `query:"sub_category_id" json:"sub_category_id"`
 }
 
 // GetTalents  used to find talent by id
@@ -53,6 +53,16 @@ func GetTalents(filter *FilteredTalent) []e.Talent {
 		query = query.Joins("JOIN expertises e ON e.id = t.expertise_id ")
 		query = query.Where("e.name ilike ? ", "%"+filter.ExpertiseName+"%")
 	}
-	query = query.Preload("Image").Offset(filter.Offset).Limit(filter.Limit).Find(&talents)
+	query = query.Preload("Services").Preload("Services.Category").Preload("Services.SubCategory").Preload("User").Preload("Image").Offset(filter.Offset).Limit(filter.Limit).Find(&talents)
 	return talents
+}
+
+// AddService used to create new service for specific talent
+func AddService(service *e.Service) error {
+	return db.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Save(&service).Error; err != nil {
+			return err
+		}
+		return nil
+	})
 }
