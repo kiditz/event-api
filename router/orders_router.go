2722a6a02f2622f2a49de25e23691ea7a2371fd5
kiditz <kiditzbastara@gmail.com>
2020-08-20 22:03:44 +0700

    Add accept invitation and reject invitation

--- router/orders_router.go
+++ router/orders_router.go
@@ -11,7 +11,8 @@ func SetOrderRoutes(v1 *echo.Group) {
 	v1.POST("/cart", c.AddCart)
 	v1.POST("/invitation", c.AddInvitation, m.IsLoggedIn(), m.IsCompany)
 	v1.GET("/invitations", c.GetInvitations, m.IsLoggedIn(), m.IsTalent)
-	v1.POST("/quotation", c.AddQuotation, m.IsLoggedIn(), m.IsTalent)
+	v1.POST("/invitation/accept", c.AcceptInvitation, m.IsLoggedIn(), m.IsTalent)
+	v1.POST("/invitation/reject", c.RejectInvitation, m.IsLoggedIn(), m.IsTalent)
 	v1.DELETE("/cart", c.DeleteCart)
 	v1.GET("/carts", c.GetCarts)
 
