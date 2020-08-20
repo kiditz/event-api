86909a1679cb804f61acc326371c5e123e3dc801
kiditz <kiditzbastara@gmail.com>
2020-08-20 22:02:15 +0700

    Add new invitation id

--- entity/quotation.go
+++ entity/quotation.go
@@ -2,11 +2,12 @@ package entity
 
 //Quotation go doc
 type Quotation struct {
-	ID         uint    `gorm:"primary_key" json:"id"`
-	ServiceID  uint    `json:"service_id" gorm:"not null;index;" validate:"required"`
-	CampaignID uint    `json:"campaign_id" gorm:"not null;index" validate:"required"`
-	Price      float64 `json:"price" validate:"required"`
-	Message    string  `json:"message" validate:"required"`
-	Status     string  `json:"status"`
+	ID           uint    `gorm:"primary_key" json:"id"`
+	ServiceID    uint    `json:"service_id" gorm:"not null;index;" validate:"required"`
+	CampaignID   uint    `json:"campaign_id" gorm:"not null;index" validate:"required"`
+	InvitationID uint    `json:"invitation_id" gorm:"index"`
+	Price        float64 `json:"price" validate:"required"`
+	Message      string  `json:"message" validate:"required"`
+	Status       string  `json:"status" validate:"required"`
 	Model
 }
